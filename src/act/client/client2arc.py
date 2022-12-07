a37bcadd75718545b7553668ebef80f7eeb2115f
jakobmerljak <merljak.jakob@gmail.com>
2022-12-07 10:30:46 +0100

    Overhaul signal handling in client module

--- src/act/client/client2arc.py
+++ src/act/client/client2arc.py
@@ -64,60 +64,69 @@ class Client2Arc(aCTProcess):
         Insert new jobs to ARC table.
 
         For now, the jobs with no arcjobid and lowest id get inserted.
+
+        Signal handling strategy:
+        - db lock needs to be released properly
+        - insertion to arcjobs and update of clientjobs need to happen
+          transactionally
+        - hence, the entire operation is protected from signals
         """
-        # client2arc races with jobmgr.killJobs for jobs that have no arcjobid
-        res = self.arcdb.db.getMutexLock('nulljobs')
-        if not res:
-            raise Exception("Could not acquire lock to insert jobs")
+        # exit handling context manager
+        with self.sigdefer:
 
-        # Get jobs that haven't been inserted to ARC table yet
-        # (they don't have reference to ARC table, arcjobid is null).
-        jobs = self.clidb.getJobsInfo(
-            ['id', 'jobdesc', 'clusterlist'],
-            where='proxyid = %s AND arcjobid IS NULL AND jobdesc IS NOT NULL',
-            where_params=[proxyid],
-            order_by='%s',
-            order_by_params=['id'],
-            limit=num
-        )
-        jobdescs = arc.JobDescriptionList()
-        for job in jobs:
+            # client2arc races with jobmgr.killJobs for jobs that have no arcjobid
+            res = self.arcdb.db.getMutexLock('nulljobs')
+            if not res:
+                raise Exception("Could not acquire lock to insert jobs")
 
-            # create downloads list
-            arc.JobDescription_Parse(job['jobdesc'], jobdescs)
-            # all files from session dir
-            downloads = ['/']
-            # all diagnose files if log dir is specified
-            logdir = jobdescs[-1].Application.LogDir
-            if logdir:
-                if logdir.endswith('/'):
-                    downloads.append(f'diagnose={logdir}')
+            # Get jobs that haven't been inserted to ARC table yet
+            # (they don't have reference to ARC table, arcjobid is null).
+            jobs = self.clidb.getJobsInfo(
+                ['id', 'jobdesc', 'clusterlist'],
+                where='proxyid = %s AND arcjobid IS NULL AND jobdesc IS NOT NULL',
+                where_params=[proxyid],
+                order_by='%s',
+                order_by_params=['id'],
+                limit=num
+            )
+            jobdescs = arc.JobDescriptionList()
+            for job in jobs:
+
+                # create downloads list
+                arc.JobDescription_Parse(job['jobdesc'], jobdescs)
+                # all files from session dir
+                downloads = ['/']
+                # all diagnose files if log dir is specified
+                logdir = jobdescs[-1].Application.LogDir
+                if logdir:
+                    if logdir.endswith('/'):
+                        downloads.append(f'diagnose={logdir}')
+                    else:
+                        downloads.append(f'diagnose={logdir}/')
+
+                # insert job to ARC table
+                try:
+                    row = self.arcdb.insertArcJobDescription(
+                        job['jobdesc'],
+                        proxyid,
+                        0,
+                        job['clusterlist'],
+                        job['id'],
+                        ';'.join(downloads)
+                    )
+                except:
+                    self.log.exception(f'Error inserting job {job["id"]} to arc table')
                 else:
-                    downloads.append(f'diagnose={logdir}/')
+                    # create a reference to job in client table
+                    self.clidb.updateJob(job['id'], {
+                        'arcjobid': row['LAST_INSERT_ID()'],
+                        'modified': self.clidb.getTimeStamp()
+                    })
+                    self.log.info(f'Successfully inserted job {job["id"]} {row["LAST_INSERT_ID()"]} to ARC engine')
 
-            # insert job to ARC table
-            try:
-                row = self.arcdb.insertArcJobDescription(
-                    job['jobdesc'],
-                    proxyid,
-                    0,
-                    job['clusterlist'],
-                    job['id'],
-                    ';'.join(downloads)
-                )
-            except:
-                self.log.exception(f'Error inserting job {job["id"]} to arc table')
-            else:
-                # create a reference to job in client table
-                self.clidb.updateJob(job['id'], {
-                    'arcjobid': row['LAST_INSERT_ID()'],
-                    'modified': self.clidb.getTimeStamp()
-                })
-                self.log.info(f'Successfully inserted job {job["id"]} {row["LAST_INSERT_ID()"]} to ARC engine')
-
-        res = self.arcdb.db.releaseMutexLock('nulljobs')
-        if not res:
-            raise Exception("Could not release lock after inserting jobs")
+            res = self.arcdb.db.releaseMutexLock('nulljobs')
+            if not res:
+                raise Exception("Could not release lock after inserting jobs")
 
     def finish(self):
         self.clidb.close()
