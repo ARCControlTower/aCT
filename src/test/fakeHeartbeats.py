7bce02c462a7ea27d2434bc6376acf9773eaf03a
David Cameron <d.g.cameron@gmail.com>
2023-03-08 22:37:59 +0100

    fix print for py3

--- src/test/fakeHeartbeats.py
+++ src/test/fakeHeartbeats.py
@@ -18,7 +18,7 @@ p = aCTPanda(logging.getLogger('test'), sys.argv[1])
 
 for cloud in ['ND', 'CA', 'CERN', 'DE', 'ES', 'FR', 'IT', 'NL', 'TW', 'UK', 'US']:
 
-    print 'Cloud', cloud
+    print (f'Cloud {cloud}')
     jobs = p.queryJobInfo(cloud=cloud)
     for j in jobs:
         if j['jobStatus'] == 'starting' or j['jobStatus'] == 'running':
