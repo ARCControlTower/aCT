e403b809eea5e38bc1d1699c69b933de34cbc6ce
jakobmerljak <merljak.jakob@gmail.com>
2023-03-20 09:45:21 +0100

    Fix validator periodic stop

--- src/act/atlas/aCTValidator.py
+++ src/act/atlas/aCTValidator.py
@@ -664,4 +664,4 @@ class aCTValidator(aCTATLASProcess):
         # Validator suffers from memory leaks in arc bindings, so exit once per day
         if time.time() - self.starttime > 60*60*24:
             self.log.info("Exiting for periodic restart")
-            self.stop()
+            self.stopWithException()
