5fa99c1713c4e2a386e44823322e0852ec330967
jakobmerljak <merljak.jakob@gmail.com>
2023-07-05 10:03:54 +0200

    Reorganize script to main function

--- src/act/common/kibana.py
+++ src/act/common/kibana.py
@@ -1,56 +1,50 @@
-#!/usr/bin/env python3
 # Generate xml and send to Kibana
 #
 # Call this in a cron with arguments service_id webpage_url
 
+import json
 import os
-import subprocess
 import sys
 import time
-import json
-import requests
 from datetime import datetime
+
+import requests
 from act.arc.aCTDBArc import aCTDBArc
 from act.atlas.aCTDBPanda import aCTDBPanda
-from act.common.aCTLogger import aCTLogger
 from act.common.aCTConfig import aCTConfigARC
+from act.common.aCTLogger import aCTLogger
 
-try:
-    service_id, webpage_url = sys.argv[1:3]
-except:
-    print('Usage: kibana.py service_id webpage_url')
-    sys.exit(1)
 
-logger = aCTLogger('kibana probe')
-log = logger()
-arcdb = aCTDBArc(log)
-pandadb = aCTDBPanda(log)
-config = aCTConfigARC()
-
-def getARCJobs():
+def getARCJobs(arcdb):
     return str(arcdb.getNArcJobs('TRUE'))
 
-def getARCSlots():
-    jobs=arcdb.getArcJobsInfo("state='Running'",['RequestedSlots'])
-    slots=0
+
+def getARCSlots(arcdb):
+    jobs = arcdb.getArcJobsInfo("state='Running'", ['RequestedSlots'])
+    slots = 0
     for j in jobs:
         slots += j['RequestedSlots']
     return str(slots)
 
-def getPandaNotStarted():
+
+def getPandaNotStarted(pandadb):
     return str(pandadb.getNJobs("actpandastatus='sent' and created<NOW()-interval 12 hour"))
 
-def getArcQueuedLong():
-    jobs=arcdb.getArcJobsInfo("state='Queuing' and created<NOW()-interval 12 hour",['id'])
+
+def getArcQueuedLong(arcdb):
+    jobs = arcdb.getArcJobsInfo("state='Queuing' and created<NOW()-interval 12 hour",['id'])
     return str(len(jobs))
 
-def getPandaDone():
+
+def getPandaDone(pandadb):
     return str(pandadb.getNJobs("actpandastatus='done'"))
 
-def getPandaDoneFailed():
+
+def getPandaDoneFailed(pandadb):
     return str(pandadb.getNJobs("actpandastatus='donefailed'"))
 
-def getAvailability():
+
+def getAvailability(config, pandadb):
 
     # Check autopilot is running
     logdir = config.logger.logdir
@@ -82,40 +76,54 @@ def getAvailability():
 def send(document):
     return requests.post('http://monit-metrics.cern.ch:10012/', data=json.dumps(document), headers={ "Content-Type": "application/json; charset=UTF-8"})
 
+
 def send_and_check(document, should_fail=False):
     response = send(document)
     assert( (response.status_code in [200]) != should_fail), 'With document: {0}. Status code: {1}. Message: {2}'.format(document, response.status_code, response.text)
 
 
-availability, desc = getAvailability()
+def main():
+    try:
+        service_id, webpage_url = sys.argv[1:3]
+    except:
+        print('Usage: kibana service_id webpage_url')
+        sys.exit(1)
 
-i=[]
-info={}
-info['producer'] = 'atlasact'
-info['type'] = "availability"
-info['availabilityinfo'] = desc
-info['service_status'] = availability
-info['availabilitydesc'] = 'Check whether aCT is functioning correctly'
-info['serviceid'] = service_id
-info['timestamp'] =  int(time.time()*1000)
-info['contact'] = 'atlas-adc-act-support@cern.ch'
-info['webpage'] = webpage_url
+    logger = aCTLogger('kibana probe')
+    log = logger()
+    arcdb = aCTDBArc(log)
+    pandadb = aCTDBPanda(log)
+    config = aCTConfigARC()
 
-infom={}
-infom['producer'] = 'atlasact'
-infom['type'] = "metric"
-infom['timestamp'] =  int(time.time()*1000)
-infom['arcjobs'] = int(getARCJobs())
-infom['arcslots'] = int(getARCSlots())
-infom['pandasent12h'] = int(getPandaNotStarted())
-infom['arcqueued12h'] = int(getArcQueuedLong())
-infom['pandadone'] = int(getPandaDone())
-infom['pandafailed'] = int(getPandaDoneFailed())
-infom['serviceid'] = service_id
-infom["idb_tags"] = ["serviceid"]
+    availability, desc = getAvailability(config, pandadb)
 
-i.append(info)
-i.append(infom)
+    i = []
+    info = {}
+    info['producer'] = 'atlasact'
+    info['type'] = "availability"
+    info['availabilityinfo'] = desc
+    info['service_status'] = availability
+    info['availabilitydesc'] = 'Check whether aCT is functioning correctly'
+    info['serviceid'] = service_id
+    info['timestamp'] = int(time.time()*1000)
+    info['contact'] = 'atlas-adc-act-support@cern.ch'
+    info['webpage'] = webpage_url
 
-send_and_check(i)
-#print(json.dumps(i))
+    infom = {}
+    infom['producer'] = 'atlasact'
+    infom['type'] = "metric"
+    infom['timestamp'] = int(time.time()*1000)
+    infom['arcjobs'] = int(getARCJobs(arcdb))
+    infom['arcslots'] = int(getARCSlots(arcdb))
+    infom['pandasent12h'] = int(getPandaNotStarted(pandadb))
+    infom['arcqueued12h'] = int(getArcQueuedLong(arcdb))
+    infom['pandadone'] = int(getPandaDone(pandadb))
+    infom['pandafailed'] = int(getPandaDoneFailed(pandadb))
+    infom['serviceid'] = service_id
+    infom["idb_tags"] = ["serviceid"]
+
+    i.append(info)
+    i.append(infom)
+
+    send_and_check(i)
+    #print(json.dumps(i))
