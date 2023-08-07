2167ee4cb88dd499bc9a9f665e9edeae198b1109
jakobmerljak <merljak.jakob@gmail.com>
2023-08-07 08:57:34 +0200

    Remove pidfile code

--- src/act/common/aCTBootstrap.py
+++ src/act/common/aCTBootstrap.py
@@ -6,6 +6,7 @@ from act.common.aCTConfig import aCTConfigARC, aCTConfigAPP
 from act.arc.aCTDBArc import aCTDBArc
 from act.condor.aCTDBCondor import aCTDBCondor
 
+
 def bootstrap_conf():
     '''Check config is ok'''
     try:
@@ -20,16 +21,13 @@ def bootstrap_conf():
         print('Error processing APP config file: %s' % str(e))
         sys.exit(1)
 
-    # Check aCT is stopped, simply checking existence of pid file
-    if os.path.exists(arcconf.actlocation.pidfile):
-        print('aCT is already running, please stop it before bootstrapping!')
-        sys.exit(1)
 
 def bootstrap_dirs():
     '''Make necessary directories'''
     arcconf = aCTConfigARC()
     os.makedirs(arcconf.tmp.dir, mode=0o755, exist_ok=True)
     os.makedirs(arcconf.logger.logdir, mode=0o755, exist_ok=True)
+
 
 def bootstrap_db():
     '''Set up the ARC and Condor DB tables'''
@@ -43,6 +41,7 @@ def bootstrap_db():
     print('Setting up Condor tables...')
     if not dbcondor.createTables():
         print('Error creating condor tables, see aCTBootstrap.log for details')
+
 
 def bootstrap_app():
     '''Set up app-specific things'''
