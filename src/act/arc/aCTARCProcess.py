5532cccb5b1be0667e298bd4465c96b581f2e606
jakobmerljak <merljak.jakob@gmail.com>
2023-08-28 12:12:52 +0200

    Set up pyarcrest logging

--- src/act/arc/aCTARCProcess.py
+++ src/act/arc/aCTARCProcess.py
@@ -1,9 +1,11 @@
 import os
+import logging
 import random
 
 from act.arc.aCTDBArc import aCTDBArc
 from act.common.aCTConfig import aCTConfigARC
 from act.common.aCTProcess import aCTProcess
+from act.common.aCTLogger import LEVELS
 from pyarcrest.arc import ARCRest
 
 
@@ -23,6 +25,18 @@ class aCTARCProcess(aCTProcess):
         super().setup()
         self.db = aCTDBArc(self.log)
 
+        # see docstring of aCTProcess.setup() for why loadConf is required
+        #
+        # the pyarcrest logs will be without cluster for now, some effort
+        # is required to add cluster URL to logs
+        self.loadConf()
+        pyarcrestLogger = logging.getLogger("pyarcrest")
+        level = LEVELS.get(self.conf.logger.level or logging.NOTSET)
+        pyarcrestLogger.setLevel(level)
+        # self.aCTLogger.LoggerAdapter.Logger.handlers
+        for handler in self.logger.logger.logger.handlers:
+            pyarcrestLogger.addHandler(handler)
+
     def finish(self):
         self.db.close()
         super().finish()
@@ -30,7 +44,7 @@ class aCTARCProcess(aCTProcess):
     def getARCClient(self, proxyid):
         proxypath = os.path.join(self.db.proxydir, f"proxiesid{proxyid}")
         try:
-            return ARCRest.getClient(url=self.cluster, proxypath=proxypath, log=self.log, timeout=900)
+            return ARCRest.getClient(url=self.cluster, proxypath=proxypath, timeout=900)
         except Exception as exc:
             self.log.error(f"Error creating REST client for proxy ID {proxyid}: {exc}")
             return None
